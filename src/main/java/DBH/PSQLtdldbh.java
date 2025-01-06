402562c88b96eeb2300fdd79590b7adb0f33e7e3
Santiago <santiagozapata2002@gmail.com>
2025-01-06 00:48:04 -0300

    Enhance database connection handling with .env and environment variable support

--- src/main/java/DBH/PSQLtdldbh.java
+++ src/main/java/DBH/PSQLtdldbh.java
@@ -6,19 +6,33 @@ import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import javax.swing.JOptionPane;
+
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class PSQLtdldbh {
     private static final HikariDataSource dataSource;
-    private static final Dotenv dotenv = Dotenv.load();
+    private static Dotenv dotenv;
 
     // Create a connection pool that reuses the same connection 
     // rather than creating a new one every time a connection is requested
     static{
         HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dotenv.get("DB_URL"));
-        config.setUsername(dotenv.get("DB_USERNAME"));
-        config.setPassword(dotenv.get("DB_PASSWORD"));
+        dotenv = Dotenv.load();
+        if(dotenv.get("DB_URL") != null){
+            JOptionPane.showMessageDialog(null, "Using .env file");
+            config.setJdbcUrl(dotenv.get("DB_URL"));
+            config.setUsername(dotenv.get("DB_USERNAME"));
+            config.setPassword(dotenv.get("DB_PASSWORD"));
+        }else if(System.getenv("DB_URL") != null){
+            JOptionPane.showMessageDialog(null, "Using environment variables");
+            config.setJdbcUrl(System.getenv("DB_URL"));
+            config.setUsername(System.getenv("DB_USERNAME"));
+            config.setPassword(System.getenv("DB_PASSWORD"));
+        }else{
+            JOptionPane.showMessageDialog(null, "No database configuration found");
+            throw new RuntimeException("No database configuration found");
+        }
         config.setMaximumPoolSize(10);
         config.setMinimumIdle(2);
         config.setIdleTimeout(30000);
